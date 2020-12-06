35f63e05abcd327d1822193b85b1a2038f871ce6
Shreyas Shriyan <51715896+shreyas-shriyan@users.noreply.github.com>
2020-12-06 23:52:37 +0530

    added edit page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom"
 import Listing from '../components/ListingPage/Listing'
 import Navbar from "../components/Navbar"
 import Details from "../components/DetailsPage/Details"
+import EditPage from "../components/EditPage/EditPage"
 
 export default function Routes() {
     return (
@@ -11,6 +12,7 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact render={(props) => <Listing {...props}></Listing>}></Route>
                 <Route path="/details/:id" exact render={(props) => <Details {...props} ></Details>}></Route>
+                <Route path="/edit/:id" exact render={(props) => <EditPage {...props}></EditPage>}></Route>
                 <Route path="*" render={() => <h1>Page Not Found</h1>}></Route>
             </Switch>
         </div>
