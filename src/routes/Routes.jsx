1c58e509767ebf16bba48bc3c9bce7c4e68dfa48
Shreyas Shriyan <51715896+shreyas-shriyan@users.noreply.github.com>
2020-12-06 21:02:25 +0530

    added details page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from "react-router-dom"
 import Listing from '../components/ListingPage/Listing'
 import Navbar from "../components/Navbar"
+import Details from "../components/DetailsPage/Details"
 
 export default function Routes() {
     return (
@@ -9,7 +10,8 @@ export default function Routes() {
             <Navbar></Navbar>
             <Switch>
                 <Route path="/" exact render={(props) => <Listing {...props}></Listing>}></Route>
-                <Route><h1>Page Not Found</h1></Route>
+                <Route path="/details/:id" exact render={(props) => <Details {...props} ></Details>}></Route>
+                <Route path="*" render={() => <h1>Page Not Found</h1>}></Route>
             </Switch>
         </div>
     )
